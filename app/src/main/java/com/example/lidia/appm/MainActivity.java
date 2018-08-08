5fc440d7b5a58ca325644a2122214dc2ba0c6580
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-08-08 15:48:56 +0000

    others

--- app/src/main/java/com/example/lidia/appm/MainActivity.java
+++ app/src/main/java/com/example/lidia/appm/MainActivity.java
@@ -5,57 +5,91 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    ImageView imageview;
+    ImageView imageview, image;
     long animationDuration=1000;
     Integer[] LEVELS;
+    String msg;
+    ImageButton button;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
+        msg = extras.getString("keyMessage");
+
+        if(msg.equals("bzero")){
+            setContentView(R.layout.activity_zero);
+        } else if(msg.equals("bone")){
         setContentView(R.layout.activity_main);
+        }else if(msg.equals("btwo")){
+        setContentView(R.layout.activity_two);
+        }else if(msg.equals("bthree")) {
+            setContentView(R.layout.activity_three);
+        }else if(msg.equals("bfour")) {
+            setContentView(R.layout.activity_four);
+        }else if(msg.equals("bfive")) {
+            setContentView(R.layout.activity_five);
+        }
         imageview=(ImageView)findViewById(R.id.plevel1);
-        LEVELS = new Integer[]{R.id.ladrillo0, R.id.ladrillo1,R.id.ladrillo2,R.id.ladrillo3, R.id.ladrillo4};
-        for(int i=0; i<=4;i++){
-            ImageButton button = (ImageButton) findViewById(LEVELS[i]);
+        image=(ImageView)findViewById(R.id.plevel);
+        LEVELS = new Integer[]{R.id.ladrillo, R.id.ladrillo0, R.id.ladrillo1,R.id.ladrillo2,R.id.ladrillo3, R.id.ladrillo4, R.id.ladrillo5};
+        for(int i=0; i<6;i++){
+            button = (ImageButton) findViewById(LEVELS[i]);
             button.setOnClickListener(this);
         }
     }
-    public void handleAnimation(View view, float xposition, float iposition){
-        ObjectAnimator animatorX =ObjectAnimator.ofFloat(imageview, "x",xposition); //225f
-        ObjectAnimator animatorY =ObjectAnimator.ofFloat(imageview, "y",iposition); //1300f
-        animatorX.setDuration(animationDuration);
-/*
-        ObjectAnimator rotateAnimation = ObjectAnimator.ofFloat(imageview, "rotation", 0f, 360f);
-        rotateAnimation.setDuration(animationDuration);
-*/
 
+    public void handleAnimation(View view, float xposition, float iposition){
+        ObjectAnimator animatorX =ObjectAnimator.ofFloat(view, "x",xposition);
+        ObjectAnimator animatorY =ObjectAnimator.ofFloat(view, "y",iposition);
+        animatorX.setDuration(animationDuration);
 
         AnimatorSet animatorSet= new AnimatorSet();
         animatorSet.playTogether(animatorX, animatorY); //rotateAnimation
         animatorSet.start();
     }
 
+    public void rotationAnimation(View view,  float xposition, float iposition){
+        ObjectAnimator animatorX =ObjectAnimator.ofFloat(imageview, "x",xposition);
+        ObjectAnimator animatorY =ObjectAnimator.ofFloat(imageview, "y",iposition);
+        animatorY.setDuration(animationDuration);
+
+        ObjectAnimator rotateAnimation = ObjectAnimator.ofFloat(imageview, "rotation", 0f, -360f);
+        rotateAnimation.setDuration(animationDuration);
+
+        AnimatorSet animatorSet= new AnimatorSet();
+        animatorSet.playTogether(animatorY,animatorX, rotateAnimation); //rotateAnimation
+        animatorSet.start();
+    }
+
     @Override
     public void onClick(View view) {
-        for(int i=0; i<=4;i++) {
 
-                if (view.getId() == LEVELS[i]) {
-                    if (i < 3) {
-                    handleAnimation(view, 250f + i * 192f, 1300f - i * 185f);
+            for(int i=0; i<6;i++) {
 
-            } else if(i==3){
-                handleAnimation(view, 250f + i * 185f, 1300f - i * 175f);
-            }else{
-                 handleAnimation(view, 250f + i * 175f, 1300f - i * 170);
+            if (view.getId() == LEVELS[i]) {
+                if( msg.equals("bfive")){
+                    ImageButton iv=(ImageButton) findViewById(LEVELS[i+1]);
+                    handleAnimation(imageview,  iv.getLeft()+20, iv.getTop()+30);
+                    handleAnimation(image,  iv.getLeft()+20, iv.getTop()+30);
+            } else if (msg.equals("bfour")){
+                    ImageButton iv=(ImageButton) findViewById(LEVELS[i+1]);
+                    handleAnimation(imageview,  iv.getLeft()+20, iv.getTop());
+                } else{
+                    ImageButton iv = (ImageButton) findViewById(LEVELS[i + 1]);
+                    handleAnimation(imageview, iv.getLeft() - 150, iv.getTop());
 
                     }
-                }
-        }}
-}
+        }
+
+            }
+            }
+    }
+
--- app/src/main/java/com/example/lidia/appm/MapActivity.java
+++ app/src/main/java/com/example/lidia/appm/MapActivity.java
@@ -1,12 +1,76 @@
 package com.example.lidia.appm;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements View.OnClickListener {
+    ImageView imageview;
+    long animationDuration=1500;
+    Integer[] BUTTONS;
+    ImageButton button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        imageview=(ImageView)findViewById(R.id.plevel1);
+        BUTTONS = new Integer[]{ R.id.bone, R.id.bthree, R.id.bzero, R.id.bfour, R.id.btwo, R.id.bfive};
+
+        for(int i=0; i<6;i++){
+            button = (ImageButton) findViewById(BUTTONS[i]);
+            button.setOnClickListener(this);
+        }
+    }
+
+    public void rotationAnimation(View view,  float xposition, float iposition){
+        ObjectAnimator animatorX =ObjectAnimator.ofFloat(imageview, "x",xposition);
+        ObjectAnimator animatorY =ObjectAnimator.ofFloat(imageview, "y",iposition);
+        animatorY.setDuration(animationDuration);
+        animatorX.setDuration(1000);
+
+        ObjectAnimator rotateAnimation = ObjectAnimator.ofFloat(imageview, "rotation", 0f, -90f);
+        rotateAnimation.setDuration(animationDuration);
+
+        AnimatorSet animatorSet= new AnimatorSet();
+        animatorSet.playTogether(animatorY,animatorX, rotateAnimation); //rotateAnimation
+        animatorSet.start();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+            for(int i=0; i<6;i++) {
+                if (view.getId() == BUTTONS[i]) {
+
+                    ImageButton iv=(ImageButton) findViewById(BUTTONS[i]);
+                    rotationAnimation(view, iv.getLeft(), iv.getTop());
+                    }
+
+
+            }
+                if(view.getId()==R.id.bzero||view.getId()== R.id.bone||view.getId()== R.id.btwo||view.getId()==R.id.bthree||view.getId()==R.id.bfour||view.getId()==R.id.bfive) {
+                    final Intent intent = new Intent(this, MainActivity.class);
+                    String msg = getResources().getResourceEntryName(view.getId());
+                    Log.i("hola", "hola" + msg);
+                    intent.putExtra("keyMessage", msg);
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            startActivity(intent);
+                        }
+                    }, 1500);
+
+                }
     }
 }
